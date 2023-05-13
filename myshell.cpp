b413c1f2117053ce8692158be5d449fd218697ed
gouyeonch <gouyeonch@naver.com>
2023-05-13 16:22:08 +0900

    myshell base code 3

--- myshell.cpp
+++ myshell.cpp
@@ -4,9 +4,11 @@
 #include <cstring>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 #include <fcntl.h>
+#include <dirent.h>
 
-void executeCommand(const std::string& command, bool runInBackground = false, int pipeTo = -1) {
+void executeCommand(const std::vector<std::string>& args, bool runInBackground = false, int pipeTo = -1) {
     pid_t pid = fork();
 
     if (pid == -1) {
@@ -31,15 +33,13 @@ void executeCommand(const std::string& command, bool runInBackground = false, in
         }
 
         // 명령어 실행
-        std::vector<char*> args;
-        char* token = std::strtok(const_cast<char*>(command.c_str()), " ");
-        while (token != nullptr) {
-            args.push_back(token);
-            token = std::strtok(nullptr, " ");
+        std::vector<char*> cArgs;
+        for (const auto& arg : args) {
+            cArgs.push_back(const_cast<char*>(arg.c_str()));
         }
-        args.push_back(nullptr);
+        cArgs.push_back(nullptr);
 
-        execvp(args[0], args.data());
+        execvp(cArgs[0], cArgs.data());
         std::cerr << "명령어를 실행할 수 없습니다." << std::endl;
         exit(EXIT_FAILURE);
     } else {
@@ -52,53 +52,237 @@ void executeCommand(const std::string& command, bool runInBackground = false, in
     }
 }
 
+void executeCommand(const std::string& command, bool runInBackground = false, int pipeTo = -1) {
+    std::vector<std::string> args;
+    std::istringstream iss(command);
+    std::string arg;
+    while (iss >> arg) {
+        args.push_back(arg);
+    }
+    executeCommand(args, runInBackground, pipeTo);
+}
+
+void listFiles(const std::string& directory) {
+    DIR* dir = opendir(directory.c_str());
+    if (dir == nullptr) {
+        std::cerr << "디렉토리를 열 수 없습니다." << std::endl;
+        return;
+    }
+
+    dirent* entry;
+    while ((entry = readdir(dir)) != nullptr) {
+        std::cout << entry->d_name << std::endl;
+    }
+
+    closedir(dir);
+}
+
+void changeDirectory(const std::string& directory) {
+    if (chdir(directory.c_str()) != 0) {
+        std::cerr << "디렉토리를 변경할 수 없습니다." << std::
+        std::cerr << "디렉토리를 변경할 수 없습니다." << std::endl;
+    }
+}
+
+std::string getCurrentDirectory() {
+    char buffer[PATH_MAX];
+    if (getcwd(buffer, PATH_MAX) == nullptr) {
+        std::cerr << "현재 디렉토리를 가져올 수 없습니다." << std::endl;
+        return "";
+    }
+
+    return std::string(buffer);
+}
+
+void createDirectory(const std::string& directory) {
+    if (mkdir(directory.c_str(), 0777) != 0) {
+        std::cerr << "디렉토리를 생성할 수 없습니다." << std::endl;
+    }
+}
+
+void removeFile(const std::string& filename) {
+    if (remove(filename.c_str()) != 0) {
+        std::cerr << "파일을 삭제할 수 없습니다." << std::endl;
+    }
+}
+
+void grep(const std::string& pattern, const std::string& filename) {
+    std::vector<std::string> args = {"grep", pattern, filename};
+    executeCommand(args);
+}
+
+void ps() {
+    std::vector<std::string> args = {"ps"};
+    executeCommand(args);
+}
+
+void killProcess(int pid) {
+    std::vector<std::string> args = {"kill", std::to_string(pid)};
+    executeCommand(args);
+}
+
+void changeFilePermissions(const std::string& filename, const std::string& permissions) {
+    std::vector<std::string> args = {"chmod", permissions, filename};
+    executeCommand(args);
+}
+
+void createTarArchive(const std::string& filename, const std::vector<std::string>& files) {
+    std::vector<std::string> args = {"tar", "-cvf", filename};
+    args.insert(args.end(), files.begin(), files.end());
+    executeCommand(args);
+}
+
+void extractTarArchive(const std::string& filename) {
+    std::vector<std::string> args = {"tar", "-xvf", filename};
+    executeCommand(args);
+}
+
+void sed(const std::string& pattern, const std::string& replacement, const std::string& filename) {
+    std::vector<std::string> args = {"sed", "-i", "s/" + pattern + "/" + replacement + "/g", filename};
+    executeCommand(args);
+}
+
+void awk(const std::string& pattern, const std::string& filename) {
+    std::vector<std::string> args = {"awk", pattern, filename};
+    executeCommand(args);
+}
+
+void curl(const std::string& url) {
+    std::vector<std::string> args = {"curl", url};
+    executeCommand(args);
+}
+
+void find(const std::string& directory, const std::string& filename) {
+    std::vector<std::string> args = {"find", directory, "-name", filename};
+    executeCommand(args);
+}
+
+void sortFile(const std::string& filename) {
+    std::vector<std::string> args = {"sort", filename};
+    executeCommand(args);
+}
+
+void renameFile(const std::string& oldname, const std::string& newname) {
+    std::vector<std::string> args = {"mv", oldname, newname};
+    executeCommand(args);
+}
+
+void moveFile(const std::string& source, const std::string& destination) {
+    std::vector<std::string> args = {"mv", source, destination};
+    executeCommand(args);
+}
+
+void copyFile(const std::string& source, const std::string& destination) {
+    std::vector<std::string> args = {"cp", source, destination};
+   
+    executeCommand(args);
+}
+
 int main() {
     std::string command;
 
-    std::cout << "쉘 프로그램을 실행합니다." << std::endl;
-
     while (true) {
-        std::cout << "명령어를 입력하세요 ('exit'를 입력하면 종료): ";
+        std::cout << "명령어 입력: ";
         std::getline(std::cin, command);
 
         if (command == "exit") {
             break;
         } else if (command.empty()) {
             continue;
-        }
-
-        // 파이프 기능 구현
-        size_t pipePos = command.find("|");
-        if (pipePos != std::string::npos) {
-            std::string firstCommand = command.substr(0, pipePos);
-            std::string secondCommand = command.substr(pipePos + 1);
-
-            int pipefd[2];
-            if (pipe(pipefd) == -1) {
-                std::cerr << "파이프를 생성할 수 없습니다." << std::endl;
-                continue;
+        } else if (command.substr(0, 3) == "ls ") {
+            std::string directory = command.substr(3);
+            listFiles(directory);
+        } else if (command.substr(0, 3) == "cd ") {
+            std::string directory = command.substr(3);
+            changeDirectory(directory);
+        } else if (command == "pwd") {
+            std::string currentDirectory = getCurrentDirectory();
+            std::cout << currentDirectory << std::endl;
+        } else if (command.substr(0, 6) == "mkdir ") {
+            std::string directory = command.substr(6);
+            createDirectory(directory);
+        } else if (command.substr(0, 3) == "rm ") {
+            std::string filename = command.substr(3);
+            removeFile(filename);
+        } else if (command.substr(0, 5) == "grep ") {
+            std::string patternAndFile = command.substr(5);
+            size_t spacePos = patternAndFile.find(' ');
+            std::string pattern = patternAndFile.substr(0, spacePos);
+            std::string filename = patternAndFile.substr(spacePos + 1);
+            grep(pattern, filename);
+        } else if (command == "ps") {
+            ps();
+        } else if (command.substr(0, 5) == "kill ") {
+            int pid = std::stoi(command.substr(5));
+            killProcess(pid);
+        } else if (command.substr(0, 6) == "chmod ") {
+            std::string args = command.substr(6);
+            size_t spacePos = args.find(' ');
+            std::string permissions = args.substr(0, spacePos);
+            std::string filename = args.substr(spacePos + 1);
+            changeFilePermissions(filename, permissions);
+        } else if (command.substr(0, 4) == "tar ") {
+            std::string args = command.substr(4);
+            size_t spacePos = args.find(' ');
+            std::string option = args.substr(0, spacePos);
+            std::string filename = args.substr(spacePos + 1);
+            if (option == "-c") {
+                std::vector<std::string> files;
+                while (std::getline(std::cin, command) && command != "") {
+                    files.push_back(command);
+                }
+                createTarArchive(filename, files);
+            } else if (option == "-x") {
+                extractTarArchive(filename);
             }
-
-            // 첫 번째 명령어 실행
-            executeCommand(firstCommand, false, pipefd[1]);
-            close(pipefd[1]);
-
-            // 두 번째 명령어 실행
-            executeCommand(secondCommand, false, pipefd[0]);
-            close(pipefd[0]);
+        } else if (command.substr(0, 4) == "sed ") {
+            std::string args = command.substr(4);
+            size_t firstSpacePos = args.find(' ');
+            size_t secondSpacePos = args.find(' ', firstSpacePos + 1);
+            std::string pattern = args.substr(0, firstSpacePos);
+            std::string replacement = args.substr(firstSpacePos + 1, secondSpacePos - firstSpacePos - 1);
+            std::string filename = args.substr(secondSpacePos + 1);
+            sed(pattern, replacement, filename);
+        } else if (command.substr(0, 4) == "awk ") {
+            std::string args = command.substr(4);
+            size_t spacePos = args.find(' ');
+            std::string pattern = args.substr(0, spacePos);
+            std::string filename = args.substr
+            awk(pattern, filename);
+        } else if (command.substr(0, 5) == "curl ") {
+            std::string url = command.substr(5);
+            curl(url);
+        } else if (command.substr(0, 5) == "find ") {
+            std::string args = command.substr(5);
+            size_t spacePos = args.find(' ');
+            std::string directory = args.substr(0, spacePos);
+            std::string filename = args.substr(spacePos + 1);
+            find(directory, filename);
+        } else if (command.substr(0, 5) == "sort ") {
+            std::string filename = command.substr(5);
+            sortFile(filename);
+        } else if (command.substr(0, 7) == "rename ") {
+            std::string args = command.substr(7);
+            size_t spacePos = args.find(' ');
+            std::string oldname = args.substr(0, spacePos);
+            std::string newname = args.substr(spacePos + 1);
+            renameFile(oldname, newname);
+        } else if (command.substr(0, 5) == "move ") {
+            std::string args = command.substr(5);
+            size_t spacePos = args.find(' ');
+            std::string source = args.substr(0, spacePos);
+            std::string destination = args.substr(spacePos + 1);
+            moveFile(source, destination);
+        } else if (command.substr(0, 3) == "cp ") {
+            std::string args = command.substr(3);
+            size_t spacePos = args.find(' ');
+            std::string source = args.substr(0, spacePos);
+            std::string destination = args.substr(spacePos + 1);
+            copyFile(source, destination);
         } else {
-            // 백그라운드 실행 기능 구현
-            bool runInBackground = false;
-            if (command.back() == '&') {
-                runInBackground = true;
-                command.pop_back();
-            }
-
-            executeCommand(command, runInBackground);
+            executeCommand(command);
         }
-
-        std::cout << std::endl;
     }
 
-    std::cout << "쉘 프로그램 종료" << std::endl;
+    return 0;
 }
